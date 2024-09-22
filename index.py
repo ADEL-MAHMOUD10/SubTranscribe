62a27ebc8bfb937f92ab15df58c5e15aff78a66e
A D E L M A H M O U D <adelam578568@gmail.com>
2024-09-23 00:31:03 +0300

    Update index.py

--- index.py
+++ index.py
@@ -10,12 +10,13 @@ from datetime import datetime
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "/tmp"
 
+
 progress = {"status": 0, "message": "Initializing"}
 
 def upload_audio_to_assemblyai(audio_path):
     headers = {"authorization": "2ba819026c704d648dced28f3f52406f"}
     base_url = "https://api.assemblyai.com/v2"
-    
+
    
     with open(audio_path, "rb") as f:
         response = requests.post(base_url + "/upload", headers=headers, data=f)
@@ -28,19 +29,11 @@ def upload_audio_to_assemblyai(audio_path):
     
    
     progress["message"] = "Uploading"
-    while progress["status"] < 25:
-        progress["status"] += 1
-        time.sleep(0.1)
-    
+    progress["status"] = 25  
+    time.sleep(0.5)
     
     progress["status"] = 50
     progress["message"] = "Processing"
-    
-   
-    while progress["status"] < 90:
-        progress["status"] += 5
-        time.sleep(0.5)
-    
     
     while True:
         transcription_result = requests.get(polling_endpoint, headers=headers).json()
@@ -52,7 +45,7 @@ def upload_audio_to_assemblyai(audio_path):
         elif transcription_result['status'] == 'error':
             raise RuntimeError(f"Transcription failed: {transcription_result['error']}")
         else:
-            time.sleep(10)  
+            time.sleep(10)
 
 @app.route('/progress')
 def progress_status():
@@ -64,36 +57,33 @@ def upload_file():
     progress = {"status": 0, "message": "Initializing"}
 
     if request.method == 'POST':
-            file = request.files['file']
-            progress["status"] += 1
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            file_extension = os.path.splitext(file_path)[1].lower()
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        file_extension = os.path.splitext(file_path)[1].lower()
 
-            if file_extension in [".mp4", ".wmv", ".mov", ".mkv", ".h.264"]:
-                video = mp.VideoFileClip(file_path)
-                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"audio_{timestamp}.mp3")
-                video.audio.write_audiofile(audio_file_path)
-                video.reader.close()
-                video.audio.reader.close_proc()
+        if file_extension in [".mp4", ".wmv", ".mov", ".mkv", ".h.264"]:
+            video = mp.VideoFileClip(file_path)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"audio_{timestamp}.mp3")
+            video.audio.write_audiofile(audio_file_path)
+            video.reader.close()
+            video.audio.reader.close_proc()
+            progress["message"] = "Converting to audio file"
+            os.remove(file_path)
+        elif file_extension in [".mp3", ".wav"]:
+            audio_file_path = file_path
+            progress["message"] = "Uploading audio file"
+        else:
+            os.remove(file_path)
+            return render_template("error.html")
 
-                progress["message"] = "Converting To Audio file"
-                os.remove(file_path)
-
-            elif file_extension in [".mp3", ".wav"]:
-                audio_file_path = file_path
-                progress["message"] = "Uploading audio file"  
-            else:
-                os.remove(file_path)
-                return render_template("error.html")
-
-            transcript_id = upload_audio_to_assemblyai(audio_file_path)
-            return redirect(url_for('download_subtitle', transcript_id=transcript_id))
+        transcript_id = upload_audio_to_assemblyai(audio_file_path)
+        return redirect(url_for('download_subtitle', transcript_id=transcript_id))
 
     return render_template('index.html')
-    
+
 @app.route('/download/<transcript_id>', methods=['GET', 'POST'])
 def download_subtitle(transcript_id):
     if request.method == 'POST':
@@ -122,24 +112,12 @@ def serve_file(filename):
     if os.path.exists(file_path):
         try:
             response = send_file(file_path, as_attachment=True)
-            
-            for _ in range(1):  
-                try:
-                    os.remove(file_path)
-                    break
-                except PermissionError:
-                    time.sleep(1)
-            
+            os.remove(file_path)  
             return response
         except Exception:
             return render_template("error.html")
-        except FileNotFoundError:
-            return render_template("error.html")
-        except TypeError:
-            return render_template("error.html")
-    
+
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True, host="0.0.0.0")
-
